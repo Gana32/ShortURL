0b975c28ce9461265a82dafe121f1d6d4faa773f
Gana32 <6ganas6553@gmail.com>
2024-02-21 12:57:48 +0530

    Url redirection added

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const urlRoute=require('./routes/url');
-
+const URL=require('./model/url');
 
 app.use(express.json());
 const PORT=3000;
 
 app.use('/url', urlRoute);
+
+app.get('/:shortId',async (req, res) => {
+    const shortId=req.params.shortId;
+    const entry= await URL.findOneAndUpdate({shortId},{
+        $push:{
+            visitHistory:{timestamp:Date.now()},
+        }
+    })
+       res.redirect(entry.longUrl); 
+})
 
 mongoose.connect('mongodb://localhost:27017/shortURL', { useNewUrlParser: true, useUnifiedTopology: true });
 
